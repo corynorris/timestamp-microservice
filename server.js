cffc4c4baf84d72037b3eba171d968733eaee357
corynorris <corynorris@gmail.com>
2016-10-30 00:12:33 -0400

    Added favicons

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var favicon = require('serve-favicon');
 var app = express();
 var port = process.env.PORT || process.argv[2] || 3000;
 
-app.use(favicon(__dirname + '/public/nodejs.xml'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function (req, res) {
   res.send(strftime(
