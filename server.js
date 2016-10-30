0511edc35031ecb7e761bc9d88da46bd96f7ce06
corynorris <corynorris@gmail.com>
2016-10-30 01:11:16 -0400

    Created the FreeCodeCamp timestamp service

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-var net = require('net');
-var strftime = require('strftime');
-var express = require('express');
 var favicon = require('serve-favicon');
+var url = require('url');
+var http = require('http');
+var moment = require('moment');
 
-var app = express();
 var port = process.env.PORT || process.argv[2] || 3000;
+var _favicon = favicon(__dirname + '/public/favicon.ico');
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+var server = http.createServer(function(req, res) {
+    _favicon(req, res, function onNext(err) {
+        var parsedUrl = url.parse(req.url, true);
+        var time = parsedUrl.pathname;
+        if (time && (time[0] === '/')) {
+            time = time.substring(1);
+        }
+        time = decodeURIComponent(time);
+        if (/^\d{8,}$/.test(time)) {
+            myDate = moment(time, "X");
+        } else {
+            myDate = moment(time, "MMMM D, YYYY");
+        }
 
-app.get('/', function (req, res) {
-  res.send(strftime(
-        "%F %H:%M\n", 
-        new Date()
-    ));
-});
+        if (myDate.isValid()) {
+            res.end(JSON.stringify({
+                unix: myDate.format("X"),
+                natural: myDate.format("MMMM D, YYYY")
+            }));
+        } else {
+            res.end(JSON.stringify({
+                unix: null,
+                natural: null
+            }));
+        }
+    });
+}).on('error', console.error);
 
-app.listen(port);
+server.listen(port);
