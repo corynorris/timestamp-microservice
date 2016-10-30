5fc852c92c71e9b24d6cf0739a4067e486d886f6
corynorris <corynorris@gmail.com>
2016-10-29 23:50:05 -0400

    Added favicon

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var net = require('net');
 var strftime = require('strftime');
 var express = require('express');
+var favicon = require('serve-favicon');
 
-var port = process.env.PORT || process.argv[2] || 3000;
 var app = express();
+var port = process.env.PORT || process.argv[2] || 3000;
+
+app.use(favicon(__dirname + '/public/nodejs.xml'));
 
 
 var server = net.createServer(function(socket) {
-    
     socket.end(strftime(
         "%F %H:%M\n", 
         new Date()
     ));
-
 });
 
 server.listen(port);
